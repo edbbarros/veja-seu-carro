aa04b46d79e8dd4927cf0b93e91a04b3c1b2345c
Eduardo Barros <39862684+edbbarros@users.noreply.github.com>
2023-05-29 16:42:22 -0300

    bug fix float menu whatsapp

--- src/components/FloatMenu.jsx
+++ src/components/FloatMenu.jsx
@@ -4,9 +4,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 const FloatMenu = () => {
   return (
     <Link
-      to={`https://wa.me/55${8000813312}?text=Adorei%20seu%20artigo`}
+      to={`https://api.whatsapp.com/send?phone=5508000813312&text=Ol%C3%A1,%20gostaria%20de%20um%20or%C3%A7amento%20para%20os%20rastreadores!%20 `} target='_blank'
       className="fixed bottom-24 right-4 z-50 flex items-center justify-center w-12 h-12 lg:w-16 lg:h-16 rounded-full text-white lg:text-4xl bg-green-500 hover:bg-green-600 focus:bg-green-600 transition-colors duration-300"
-      target="_blank"
     >
       <i className="fab fa-whatsapp "></i>
     </Link>
